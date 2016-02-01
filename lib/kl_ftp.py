ced7deac47f311a99b6f43870525df1a6404db1e
kelicom <735579768@qq.com>
2016-02-01 22:15:32 +0800

    加入ssh

--- lib/kl_ftp.py
+++ lib/kl_ftp.py
@@ -1,4 +1,4 @@
-import ftplib, os , kl_log
+import ftplib, os , kl_log, paramiko
 
 #定义匿名函数
 #打开一个文件句柄
@@ -9,25 +9,47 @@ getcwd = lambda curwd: curwd == '/' and '/' or (curwd)
 createDir = lambda dirname: not os.path.exists(dirname) and os.makedirs(dirname)
 
 class kl_ftp:
-    def __init__(self,host,port,username,password):
-        self.ftp = ftplib.FTP()
+    def __init__(self,host,port,username,password,ftptype='ftp'):
+        self.ftptype=ftptype
+        self.ssh=None
+        self.sftp=None
+        self.ftp = None
         self.ignorefolder=[]
         self.faillist=[]
         self.localroot='./'
+        if ftptype=='ftp':
+            self.__ftpconn(host,port,username,password)
+        else:
+            self.__sftpconn(host,username,password)
+        #初始化日志
+        self.log=kl_log.kl_log('kl_ftp')
+
+    def __ftpconn(self,host,port,username,password):
+        self.ftp=ftplib.FTP()
         #最大1G文件
         self.ftp.maxline=1024*1024*1024
         #f.encoding='UTF-8'#防止中文乱码
         self.ftp.connect(host,port)
-        resp = self.ftp.login(username, passewd)
+        resp = self.ftp.login(username, password)
         #输出欢迎信息
         print(resp)
-        #初始化日志
-        self.log=kl_log.kl_log('kl_ftp')
+
+    #定义 ssh 连接函数
+    def __sftpconn(self,_host,_username='',_password=''):
+        try:
+            self.ssh= paramiko.SSHClient()
+            self.ssh.set_missing_host_key_policy( paramiko.AutoAddPolicy() )
+            self.ssh.connect(_host,username=_username,password=_password)
+            self.sftp=self.ssh.open_sftp()
+        except Exception as e:
+            print( 'ssh %s@%s: %s' % (_username,_host, e) )
+            exit()
 
     #判断是否是目录
     def isDirectory(self,filename):
         try:
-            self.ftp.cwd(filename)
+            if self.ftptype=='ftp':
+                self.ftp.cwd(filename)
             createDir(self.localroot+filename)
             return True
         except ftplib.error_perm as e:
@@ -76,16 +98,34 @@ class kl_ftp:
     def uploadfolder(self,localroot,folder):
         pass
 
+    def s_downloadfolder(self,folder,localroot):
+        self.localroot=localroot
+        createDir(self.localroot)
+        self.sftp.get(folder,localroot+'i.php')
+
+    def ssh_command(self,command):
+        stdin, stdout, stderr = self.ssh.exec_command(command)
+        print stdout.readlines()
+
     def close(self):
-        self.ftp.quit()
+        if self.ftp!=None:
+            self.ftp.quit()
+        if self.sftp!=None:
+            self.sftp.close()
+        if self.ssh!=None:
+            self.ssh.close()
 
 if __name__ == '__main__':
-    local_root = 'E:/ftp'
-    host = '116.255.214.72'
-    username = 'wwwroot'
-    passewd = 'adminrootkl'
-    ftp=kl_ftp(host,2016,'wwwroot','adminrootkl')
-    ftp.ignorefolder=['Data', 'Public', 'App', 'Plugins', 'TP','dflz.zip']
-    ftp.downloadfolder('test','E:/ftp/')
-    ftp.close()
+    # local_root = 'E:/ftp'
+    # host = '116.255.214.72'
+    # username = 'wwwroot'
+    # passewd = 'adminrootkl'
+    # ftp=kl_ftp(host,2016,'wwwroot','adminrootkl')
+    # ftp.ignorefolder=['Data', 'Public', 'App', 'Plugins', 'TP','dflz.zip']
+    # ftp.downloadfolder('test','E:/ftp/')
+    # ftp.close()
+
+    #连接ssh服务器
+    sftp=kl_ftp('116.255.159.47', 25,'root', 'adminrootkl','sftp')
+    sftp.s_downloadfolder('/var/www/zhaokeli.com/index.php', 'E:/sftp/')
     input('请输入任意键结束...')
