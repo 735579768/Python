f5aa4c703645bd91acce38ff5ed38d5474342e62
kelicom <735579768@qq.com>
2016-03-20 21:50:26 +0800

    添加抓取数据

--- lib/kl_spider/datadb.py
+++
@@ -1,5 +0,0 @@
-class Datadb(object):
-    """docstring for Datadb"""
-    def __init__(self, arg):
-        super(Datadb, self).__init__()
-        self.arg = arg
--- lib/kl_spider/internet.py
+++
@@ -1,5 +0,0 @@
-class Internet(object):
-    """docstring for internet"""
-    def __init__(self, arg):
-        super(Internet, self).__init__()
-        self.arg = arg
